76a10d703f8fcebe01b5485b92bef3f9c1dd4ce7
Bob van den Hoogen <bob@strmprivacy.io>
2022-05-24 17:28:26 +0200

    fix: add syntax highlighting for rust

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['typescript', 'java', 'php'],
+        additionalLanguages: ['typescript', 'java', 'php', 'rust'],
       },
       algolia: {
         appId: 'M24OHV52F0',
