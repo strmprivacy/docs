3e419ae995ffccf082d29a5cacd7b14d05181940
Robin Trietsch <robin@strmprivacy.io>
2022-10-11 09:02:07 +0200

    chore: add extra redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 // TODO remove the redirects from the old pages, we should update all GH repositories to link to new docs.
 // This ensures URLs to our old Antora-based docs are successfully redirected to Docusaurus-based URLs
-let oldVersions = ["latest", "0.21.0", "0.20.1", "0.20.0", "0.19.0", "0.18.0", "0.17.0", "0.16.0", "0.15.0", "0.14.0", "0.13.2", "0.13.1", "0.13.0", "0.12.0", "0.11.0", "0.10.0", "0.9.0", "0.8.0", "0.7.0", "0.6.0", "0.5.1", "0.5.0", "0.4.1"]
+let oldVersions = ["latest", "0.21.0", "0.20.1", "0.20.0", "0.19.0", "0.18.0", "0.17.0", "0.16.0", "0.15.0", "0.14.0", "0.13.2", "0.13.1", "0.13.0", "0.12.0", "0.11.0", "0.10.0", "0.9.0", "0.8.0", "0.7.0", "0.6.0", "0.5.1", "0.5.0", "0.4.1", "0.3.1"]
 let oldOverviewPages = ["pii", "organization", "hla"];
 let replacements = [];
 
@@ -26,6 +26,11 @@ for (let oldVersion of oldVersions) {
   replacements.push({
     from: `/docs/${oldVersion}/definitions.html`,
     to: `/docs/latest/concepts/definitions`,
+  });
+
+  replacements.push({
+    from: `/docs/${oldVersion}/concepts/schemas-and-contracts.html`,
+    to: `/docs/latest/concepts/data-contracts/`,
   });
 }
 
