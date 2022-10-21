fe9d5814912d691e2e5d9454fb0c7ab938f8cbdd
Robin Trietsch <robin@strmprivacy.io>
2022-10-21 13:23:34 +0200

    chore: add docs for code block placeholders

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -1,6 +1,24 @@
 import React, {useState} from 'react';
 import CodeBlock from '@theme-original/CodeBlock';
 
+/*
+Inspired by Google Cloud Documentation's 'Format Placeholders' (https://developers.google.com/style/placeholders)
+this implements a Markdown compatible way of defining and using placeholders in code blocks, that uses can edit
+
+Using the placeholders can be done as follows:
+
+```json title=my-json.json placeholders my_placeholder=Text that will be shown in the label, another_one=This is cool!
+{
+    "someKey": "Using placeholders is simple, just fill out a text in the input, and it'll be shown here: $my_placeholder",
+    "someOtherKey": "Did I say this already? Any user value is shown here = $another_one"
+}
+```
+
+Important remark, the `placeholders` keyword in the metastring, is used as a separator. Everything after the `placeholders`
+keyword is considered to be a placeholder. The key of a placeholder can be used in the code block string and the value
+is used as the label shown next to the input field.
+*/
+
 function stateManagement(placeholders) {
   let initialState = Object.fromEntries(Object.entries(placeholders).map(([key, prop]) => [key, prop.inputPlaceholder]));
   let [placeholderValues, setPlaceholderValues] = useState(initialState);
